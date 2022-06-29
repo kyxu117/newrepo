6fb0eda0cba620842b5af4cbd8a96d41ed27932d
Hazel <restrest169@gmail.com>
2022-06-29 20:28:19 +0800

    first

--- untitled1.py
+++ untitled1.py
@@ -1,8 +1,3 @@
-import yfinance as yf
-stock = yf.Ticker('GOOG')
-# 取得各種資料
-#print(stock.info) # 取得公司資料
-print(stock.financials) # 取得損益表
-stock.balance_sheet # 取得資產負債表
-stock.cashflow # 取得現金流量表
-stock.history # 取得價量資料＋股利發放資料＋股票分割資料
+x = 123
+y = 234
+print(x+y)
